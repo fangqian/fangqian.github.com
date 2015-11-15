59b6bf1e49317b7696c935ad81e1cca8c3a2cb85
fangqian <qiangu_fang@163.com>
2015-11-16 00:45:44 +0800

    update

---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+orgnote-emacs-el
+================
+
+A sit of emacs el file for [OrgNote](https://github.com/LeslieZhu/OrgNote)
+
+In [OrgNote](https://github.com/LeslieZhu/OrgNote) blog root dir, run:
+
+```
+$ git clone git@github.com:LeslieZhu/orgnote-emacs-el.git scripts/
+```
+
---
+++ scripts/init-orgnote.el
@@ -0,0 +1,15 @@
+;; 设置org-mode输出中文目录
+(custom-set-variables
+ '(org-blank-before-new-entry 
+   (quote ((heading) (plain-list-item))))
+
+ '(org-export-language-setup 
+   (quote 
+    (("en" "Author" "Date" "Table of Contents" "Footnotes") ("zh-CN" "作者" "日期" "目录" "脚注")))))
+
+
+;;; load htmlize.el
+(add-to-list 'load-path (file-name-directory "./scripts/"))
+(require 'htmlize)
+
+ 
